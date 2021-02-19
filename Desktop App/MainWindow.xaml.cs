f168b1164d9a99da4ce6b97c7502b19ba4d3afce
Peter <peterliessem.2@gmail.com>
2021-02-19 15:08:22 +0100

    Made a visual thing in the app :)

--- Desktop App/MainWindow.xaml.cs
+++ Desktop App/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,30 @@ namespace Desktop_App
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    public partial class MainWindow : Window    {
+
+        private Hotel_ManagerEntities dx = new Hotel_ManagerEntities();
+
+        private DbSet<room> rooms;
+        private DbSet<booking> bookings;
+        private DbSet<cleanRequest> cleanRequests;
+        private DbSet<maintainenceRequest> maintainenceRequests;
+        private DbSet<roomService> roomServices;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            rooms = dx.rooms;
+            bookings = dx.bookings;
+            cleanRequests = dx.cleanRequests;
+            maintainenceRequests = dx.maintainenceRequests;
+            roomServices = dx.roomServices;
+
+            rooms.Load();
+
+            roomList.DataContext = rooms.Local;
+
         }
     }
 }
