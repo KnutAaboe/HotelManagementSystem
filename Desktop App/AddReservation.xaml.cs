912294e3d05c3ec62c6a9665f69ab7e8af55e9a5
Peter <peterliessem.2@gmail.com>
2021-02-22 15:40:36 +0100

    Fixed reservation adder

--- Desktop App/AddReservation.xaml.cs
+++ Desktop App/AddReservation.xaml.cs
@@ -34,13 +34,51 @@ namespace Desktop_App
         private void addResBtn_Click(object sender, RoutedEventArgs e)
         {
             booking b = new booking();
-            if (int.TryParse(roomNrBox.Text, out int roomnr) && 0>=DateTime.Compare(startDate.SelectedDate.Value, endDate.SelectedDate.Value)){
-                responseBox.Text = "Reservation made for room: " + roomnr;
-                roomNrBox.Text = "";
-            }
-            else
+            try
             {
-                responseBox.Text = "Error making reservation.";
+                int.TryParse(roomNrBox.Text, out int roomnr);
+                if (roomnr != 0 &&
+                    phoneNr.Text.Length == 8 && 
+                    int.TryParse(phoneNr.Text, out _) &&
+                    startDate != null && 
+                    endDate != null && 
+                    0 >= DateTime.Compare(startDate.DisplayDate, endDate.DisplayDate) &&
+                    dx.rooms.Where(r => r.roomNr == roomnr).Count() != 0)
+                {
+                    responseBox.Content = "Reservation made for room: " + roomnr;
+                    roomNrBox.Text = "";
+
+                    dx.bookings.Load();
+
+                    int ID = dx.bookings.Local.Count;
+                    while(dx.bookings.Local.Where(book => book.ID == ID).Count() != 0)
+                    {
+                        ID++;
+                    }
+                    responseBox.Content += "\nReservation ID: " + ID;
+                    b.ID = ID;
+                    b.phoneNr = phoneNr.Text;
+                    b.roomNr = roomnr;
+                    b.startTime = startDate.DisplayDate;
+                    b.endTime = endDate.DisplayDate;
+
+                    dx.bookings.Add(b);
+                    dx.SaveChanges();
+
+                    phoneNr.Text = roomNrBox.Text = "";
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+            catch(System.Data.Entity.Infrastructure.DbUpdateException ex)
+            {
+                responseBox.Content = "Error making reservation.\nNo user with that phone number.";
+            }
+            catch
+            {
+                responseBox.Content = "Error making reservation.";
             }
         }
     }
